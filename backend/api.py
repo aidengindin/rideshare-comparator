ee9c03a1382c8e27a1780851163082982747f3d2
Aiden Gindin <aiden@thegindins.com>
2021-04-18 14:54:35 -0400

    added command-line option to choose whether or not to use HTTPS

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,17 @@
 import flask
-# from flask import request
 
 import json
 import sys
 
-PORT = 5000    # port to run the server on
-DEBUG = False  # whether to run in debug mode
+PORT = 5000         # port to run the server on
+DEBUG = False       # whether to run in debug mode
+SSL_CONTEXT = None  # indicates whether to use HTTPS
 
 if "--debug" in sys.argv:
     DEBUG = True
+
+if "--https" in sys.argv:
+    SSL_CONTEXT = "ahdoc"
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = DEBUG
@@ -23,4 +26,4 @@ def home():
     return json.dumps(response), {"Content-Type": "application/json"}
 
 if __name__ == "__main__":
-    app.run(port=PORT, ssl_context="adhoc")
+    app.run(port=PORT, ssl_context=None)
