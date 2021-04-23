cf44fb054bf5868f3bd0ca7992db63a7822b915e
Rbower123 <Reedlbower@gmail.com>
2021-04-23 12:15:39 -0500

    Current time working
    Generation of request is set up.

--- backend/DatabaseTest.py
+++ backend/DatabaseTest.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import datetime as dt
+import time
 
 
 def database_function(scrLat, srcLon, destLat, destLon): #, results_list): 
@@ -12,29 +13,39 @@ def database_function(scrLat, srcLon, destLat, destLon): #, results_list):
         )
     mc = mydb.cursor()
     
-    
-    # Template code to insert data into tables
-    # q1 = mc.execute("INSERT INTO request (srcLat, srcLon, destLat, destLon, date, time) VALUES(%s,%s,%s,%s,%s,%s)", (srcLat, srcLon, destLat, destLon, date, time)
-    # q2 = mc.execute("INSERT INTO ride (name, price, seats, shared, pickup, arrival) VALUES(%s,%s,%s,%s,%s,%s)", (name, price, seats, shared, pickup, arrival)
-    # q3 = mc.execute("INSERT INTO generates(reqid, rid) VALUES(%s, %s)",(lastreqid, lastrid))    
+    """
+     Template code to insert data into tables
+     q1 = mc.execute("INSERT INTO request (srcLat, srcLon, destLat, destLon, date, time) VALUES(%s,%s,%s,%s,%s,%s)", (srcLat, srcLon, destLat, destLon, date, time)
+     q2 = mc.execute("INSERT INTO ride (name, price, seats, shared, pickup, arrival) VALUES(%s,%s,%s,%s,%s,%s)", (name, price, seats, shared, pickup, arrival)
+     q3 = mc.execute("INSERT INTO generates(reqid, rid) VALUES(%s, %s)",(lastreqid, lastrid))    
+    """
 
-    #mc.execute("INSERT INTO request(srcLat, srcLon, destLat, destLon, date, time) VALUES(%s,%s,%s,%s,%s,%s)", (100.231,243.21, 200.451, 354.213, '2021-4-12', '18:25:00'))
-    #lastreqid = mc.lastrowid
-    #mc.execute("INSERT INTO ride(name, price, seats, shared, pickup, arrival) VALUES(%s,%s,%s,%s,%s,%s)", ('pool', 32.31, 3, 0, '20:59:59', '23:59:59'))
-    #lastrid = mc.lastrowid
-    #mc.execute("INSERT INTO generates(reqid, rid) VALUES(%s, %s)",(lastreqid, lastrid))
-
-    #mydb.commit()
-    date = dt.date.today()
-   # time = dt.now().time()
+    # Inserting the request, rides, and generates
+    """
     mc.execute("INSERT INTO request(srcLat, srcLon, destLat, destLon, date, time) VALUES(%s,%s,%s,%s,%s,%s)", (100.231,243.21, 200.451, 354.213, '2021-4-12', '18:25:00'))
+    lastreqid = mc.lastrowid
+    
+    FOR LOOP-as many entries in results_list
+        mc.execute("INSERT INTO ride(name, price, seats, shared, pickup, arrival) VALUES(%s,%s,%s,%s,%s,%s)", ('pool', 32.31, 3, 0, '20:59:59', '23:59:59'))
+        lastrid = mc.lastrowid
+        mc.execute("INSERT INTO generates(reqid, rid) VALUES(%s, %s)",(lastreqid, lastrid))
+    """
+    
+    #mydb.commit()
+
+    #Current date and time for request made
+    date = dt.date.today()
+    t = time.localtime()
+    now = time.strftime("%H:%M:%S", t)
+    
+    mc.execute("INSERT INTO request(srcLat, srcLon, destLat, destLon, date, time) VALUES(%s,%s,%s,%s,%s,%s)", (scrLat, srcLon, destLat, destLon, date, t))
 
 
     #Clearing the specified table - for testing purposes
     #mc.execute("DELETE FROM request")
     #mc.execute("DELETE FROM ride")
     #mc.execute("DELETE FROM generates")
-
+    
     mc.execute("SELECT * FROM request")
     for x in mc:
         print(x)
