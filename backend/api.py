bf858450fe99626185b8a9c20dad2c1884c734f8
Aiden Gindin <aiden@thegindins.com>
2021-04-27 17:24:30 -0400

    functions to get data from Uber and Lyft that return dummy data

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,9 @@ import flask
 from flask import request
 
 import asyncio
+import datetime
 import json
+import random
 import requests
 import sys
 
@@ -79,8 +81,81 @@ async def get_route(srclat, srclon, destlat, destlon):
         return response.json()
     return generateError("MapQuest API responded with an error: " + response.reason)
 
+# Get available rides from Uber
+# Since we don't currently have Uber API access, this just generates dummy data
+async def get_uber_rides(srclat, srclon, destlat, destlon):
+    return [
+        {
+            "provider": "Uber",
+            "name": "UberX",
+            "pickup": random_time_in_range(5, 10),
+            "arrival": random_time_in_range(15, 30),
+            "price": random.randrange(15, 25),
+            "seats": 4,
+            "shared": False
+        },
+        {
+            "provider": "Uber",
+            "name": "UberXL",
+            "pickup": random_time_in_range(5, 10),
+            "arrival": random_time_in_range(15, 30),
+            "price": random.randrange(25, 35),
+            "seats": 6,
+            "shared": False
+        },
+        {
+            "provider": "Uber",
+            "name": "Uber Pool",
+            "pickup": random_time_in_range(5, 10),
+            "arrival": random_time_in_range(15, 30),
+            "price": random.randrange(5, 15),
+            "seats": 4,
+            "shared": True
+        }
+    ]
+
+# Get available rides from Lyft
+# Since we don't currently have Uber API access, this just generates dummy data
+async def get_uber_rides(srclat, srclon, destlat, destlon):
+    return [
+        {
+            "provider": "Lyft",
+            "name": "Lyft",
+            "pickup": random_time_in_range(5, 10),
+            "arrival": random_time_in_range(15, 30),
+            "price": random.randrange(15, 25),
+            "seats": 3,
+            "shared": False
+        },
+        {
+            "provider": "Lyft",
+            "name": "Lyft XL",
+            "pickup": random_time_in_range(5, 10),
+            "arrival": random_time_in_range(15, 30),
+            "price": random.randrange(25, 35),
+            "seats": 5,
+            "shared": False
+        },
+        {
+            "provider": "Lyft",
+            "name": "Lyft Shared",
+            "pickup": random_time_in_range(5, 10),
+            "arrival": random_time_in_range(15, 30),
+            "price": random.randrange(5, 15),
+            "seats": 4,
+            "shared": True
+        }
+    ]
+
+# Convert a lat/lon pair to a string
 def locstring(lat, lon):
     return str(lat) + "," + str(lon)
+
+# Choose a time uniformly at random between (current time + start_offset) and (current_time + end_offset)
+# Offsets are specified in minutes
+def random_time_in_range(start_offset, end_offset):
+    offset = datetime.timedelta(seconds=(random.randrange(start_offset, end_offset) * 60))
+    return datetime.datetime.now() + offset
 
 if __name__ == "__main__":
     app.run(port=PORT, ssl_context=None)
